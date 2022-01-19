66ef005fbe0787d1e8cc0b143a50fce08466a5a8
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2022-01-20 00:45:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -185,7 +185,8 @@ client.on("message", message => {
         })
     })
     }
-  }                                     
+  })
+
 });
       client.on("message", message => { 
   if (message.content.startsWith(`${prefix}sort`)) {
