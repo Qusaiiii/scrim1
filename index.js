42cd70828cd61d84853126d31529de9b6291c7ff
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2022-01-20 00:46:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -176,17 +176,12 @@ client.on("message", message => {
           json[message.guild.id].slot++;
           writeFileSync("./json.json", JSON.stringify(json, null, 2));
         
-        embed.fields.sort(function (a, b) {
-            if (a.one < b.two) return 1;
-        
+   
         
          msg.edit(embed);      
 
         })
-    })
-    }
-  })
-
+  }
 });
       client.on("message", message => { 
   if (message.content.startsWith(`${prefix}sort`)) {
