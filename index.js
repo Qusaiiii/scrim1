4838a74c66a4a1c7ccb35c7d186aa8b4ad472005
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2022-01-19 16:42:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ const { writeFileSync, readFileSync } = require("fs");
       let json = JSON.parse(readFileSync('./json.json', "utf-8"));
 client.on("message", message => { 
   if (message.content.startsWith(`${prefix}checkin`)) {
-    if(json[message.guild.id]?.slot >= 16) return message.channel.send(`Lobby 1 IS FULL!, use C2`);
+    if(json[message.guild.id]?.slot >= 17) return message.channel.send(`**Lobby 1 IS FULL!, use 1checkin2`);
              let s = (message.member.roles.cache.find(r => r.name === "Tier1") || message.member.roles.cache.find(r => r.name === "Tier2")) 
               
       
