89b989411fadfbe2394cd55227d55501d3df6da7
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2022-01-23 01:06:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -129,34 +129,7 @@ client.on("message", message => {
       
  }
 });
-client.on("message", message => { 
-  if(message.content.startsWith(prefix+"ste")) {//  بدء التقديم
-  
-  let aceRoom = message.guild.channels.cache.find(ch => ch.id === '932431084132646942')
-                      
-  func: (cmd, args, msgObj, speaker, channel, guild) => {
-        const createdStr = Util.getDateString(guild.createdAt);
 
-        const sendEmbedFields = [];
-
-        sendEmbedFields.push({ name: 'ID', value: guild.id });
-        sendEmbedFields.push({ name: 'Name', value: guild.name });
-        sendEmbedFields.push({ name: 'Owner', value: guild.owner.toString() });
-        sendEmbedFields.push({ name: 'Region', value: Util.capitalize(guild.region) });
-        sendEmbedFields.push({ name: 'Members', value: guild.memberCount });
-        sendEmbedFields.push({ name: 'Text Channels', value: Util.getTextChannels(guild).size });
-        sendEmbedFields.push({ name: 'Voice Channels', value: Util.getVoiceChannels(guild).size });
-        sendEmbedFields.push({ name: 'Roles', value: guild.roles.size });
-        sendEmbedFields.push({ name: 'Default Channel', value: Util.capitalize(guild.defaultChannel.name) });
-        sendEmbedFields.push({ name: 'AFK Timeout', value: `${guild.afkTimeout} seconds` });
-        sendEmbedFields.push({ name: 'Created', value: createdStr });
-        sendEmbedFields.push({ name: 'Icon', value: guild.iconURL('png') });
-
-        sendEmbedFields.sort((a, b) => (String(a.name) + String(a.value)).length - (String(b.name) + String(b.value)).length);
-
-        Util.sendEmbed(channel, 'Guild Info', null, Util.makeEmbedFooter(speaker), guild.iconURL, colGreen, sendEmbedFields);
-    },
-});         
     
   client.on("message", message => { 
   if(message.content.startsWith(prefix+"ste")) {//  بدء التقديم
