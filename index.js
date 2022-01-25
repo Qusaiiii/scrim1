1c15ac52146de37a738dbd3a3d3d5406bd9171ee
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2022-01-25 13:27:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ let msg2 = '935300162656546889';
         
     channel.messages.fetch(msg2).then(msg2 => {
         let embed2 = msg2.embeds[0];
-          if (s2) embed2.addField(`> \`SLOT ${json[message.guild.id].slot}:\``, `${arg}` + `\`『 ${s} 』\``)
+          if (s2) embed2.addField(`> \`SLOT ${json[message.guild.id].slot}:\``, `${arg}` + `『 ${s} 』`)
         
           json[message.guild.id].slot++;
           writeFileSync("./json.json", JSON.stringify(json, null, 2));
@@ -203,7 +203,7 @@ let msg2 = '935300162656546889';
         
                     channel.messages.fetch(msg3).then(msg3 => {
       let embed3 = msg3.embeds[0];
-          if (s3) embed.addField(`> \`SLOT ${json[message.guild.id].slot}:\``, `${arg}` + `\『 ${s} 』\`)
+          if (s3) embed.addField(`> \`SLOT ${json[message.guild.id].slot}:\``, `${arg}` + `${s}`)
                 // ADD
             msg3.edit(embed3); 
                         
